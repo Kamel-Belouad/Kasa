696d03f56e094ac2d581a274eae5a11d6822d765
Kamel <kamelbelouad15@gmail.com>
2025-10-29 16:53:54 +0100

    Récupération des propriétés de l'API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,26 @@
 import seaImage from "../assets/img/sea-image.png"
-
-
-
+import { PropertiesApi } from "../api/PropertiesApi"
+import { Link } from "react-router-dom";
 export function Home(){
+    const locations = PropertiesApi();
     
     return <>
         <div>
             <img src={seaImage} alt="first image"/>
             <div>Chez vous, partout et ailleurs</div>
         </div>
+        
+        {locations.map((location) => {
+            return  (
+         
+                <div>
+                    <img src={location?.pictures[0]} alt="" />
+                    <div>{location.title}</div>
+                </div>
+           
+            )
+        })}
+       
         
     </>
 }
