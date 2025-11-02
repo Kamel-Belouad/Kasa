bb32bf53ac8329556ed36e8a285ffc9a4a797ecd
Kamel <kamelbelouad15@gmail.com>
2025-11-02 18:01:43 +0100

    Mise à jour du style de la page d'accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,32 @@
 import seaImage from "../assets/img/sea-image.png"
 import { PropertiesApi } from "../api/PropertiesApi"
 import { Link } from "react-router-dom";
+import "../styles/home.css"
+
 export function Home(){
     const locations = PropertiesApi();
     
     return <>
-        <div>
+        <div className="image-container"> 
             <img src={seaImage} alt="first image"/>
-            <div>Chez vous, partout et ailleurs</div>
+            <div className="text-overlay">Chez vous, partout et ailleurs</div>
         </div>
-        
+
+        <div className="locations-container">
         {locations.map((location) => {
             return  (
-            <Link to={`/location/${location.id}`}>                         
-                <div>
-                    <img src={location?.pictures[0]} alt="" />
-                    <div>{location.title}</div>
+            <Link key={location.id} to={`/location/${location.id}`} className="removeUnderLine">                         
+                <div className="location-card">
+              
+                    <img className="image-card" src={location?.pictures[0]} alt="" />
+                    <div className="title-card">{location.title}</div>
+               
                 </div>
+                
             </Link>
             )
         })}
-       
+       </div>
         
     </>
 }
