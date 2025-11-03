a38ada19e2df457e16c40d20d7cd324a63655887
Kamel <kamelbelouad15@gmail.com>
2025-11-03 21:49:59 +0100

    Style de la page d'erreur

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,18 +1,20 @@
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
+import { Link } from "react-router-dom"
+import "../styles/404.css"
 
-export const ErrorPage = ()=>{
-    return <>
-    <Header/>
-    <main>
-        <div>
-            <h1>404</h1>
-            <p>Oups ! La page que vous demandez n'existe pas</p>
-            <button>Retourner sur la page d'accueil</button>
-        </div>
-    </main>
-   
-    <h1>ERROR 404</h1>
-    <Footer/>
-    </>
+export const ErrorPage = () => {
+    return (
+        <>
+            <Header />
+            <main className="error-page">
+                <div className="error-container">
+                    <h1 className="error-code">404</h1>
+                    <p className="error-message">Oups ! La page que vous demandez n'existe pas</p>
+                    <Link to="/" className="error-link">Retourner sur la page d'accueil</Link>
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
 }
