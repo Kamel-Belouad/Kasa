242515589d81ae06380c02039cb0af2c84c2a138
Kamel <kamelbelouad15@gmail.com>
2026-01-18 16:18:15 +0100

    Mise à jour du style et la  responsive de la page d'erreur

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,7 +10,7 @@ export const ErrorPage = () => {
             <main className="error-page">
                 <div className="error-container">
                     <h1 className="error-code">404</h1>
-                    <p className="error-message">Oups ! La page que vous demandez n'existe pas</p>
+                    <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
                     <Link to="/" className="error-link">Retourner sur la page d'accueil</Link>
                 </div>
             </main>
