b4adafafb960be6e12b9f5ced13134a09c6d8832
Kamel <kamelbelouad15@gmail.com>
2026-01-18 16:46:56 +0100

    Retour à la ligne du message d'erreur en version mobile de la page d'erreur

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,7 +10,10 @@ export const ErrorPage = () => {
             <main className="error-page">
                 <div className="error-container">
                     <h1 className="error-code">404</h1>
-                    <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+                    <p className="error-message">
+                        Oups! La page que <span className="line-break"><br /></span> 
+                        vous demandez n'existe pas.
+                    </p>
                     <Link to="/" className="error-link">Retourner sur la page d'accueil</Link>
                 </div>
             </main>
