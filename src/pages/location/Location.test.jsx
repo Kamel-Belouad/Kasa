2c56ca3984fb620af94d19ac95fdde3928f0859a
Kamel <kamelbelouad15@gmail.com>
2026-02-08 19:22:49 +0100

    Test(Location Page): redirection vers la page 404 si ID  non valide

--- src/pages/location/Location.test.jsx
+++ src/pages/location/Location.test.jsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import { Location } from "./Location";
-import { PropertiesApiById } from "../../Api/PropertiesApiById";
+
 
 jest.mock("../../Api/PropertiesApiById", () => ({
     PropertiesApiById: jest.fn((id) => {
         if (id === "c67ab8a7") {
             return{
                 id:"c67ab8a7",
-                title:"Appartement cosy",
+                title:"Appartement cosy",  
                 location: "Ile de France - Paris 17e",
             };
         }
@@ -16,6 +16,7 @@ jest.mock("../../Api/PropertiesApiById", () => ({
     }),
 }));
 
+describe("Location Page", () => {
     test("Vérifier que le titre et la localisation de location sont bien affichés ",() => {
         render (
             <MemoryRouter initialEntries={["/location/c67ab8a7"]}>
@@ -30,6 +31,18 @@ jest.mock("../../Api/PropertiesApiById", () => ({
 
         const locationZone = screen.getByText("Ile de France - Paris 17e");
         expect (locationZone).toBeInTheDocument();
-
     
     });
+    test ("redirection vers la page 404 au cas ou la location n'existe pas", () =>{
+        render(
+            <MemoryRouter initialEntries={["/location/error-id"]}>
+                <Routes>
+                    <Route path="/location/:id" element={<Location />} />
+                    <Route path="/errorPage" element = {<div>Page d'erreur 404</div>} />
+                </Routes>
+            </MemoryRouter>
+        );
+        const errorPage =screen.getByText("Page d'erreur 404");
+        expect (errorPage).toBeInTheDocument();
+    });
+});
